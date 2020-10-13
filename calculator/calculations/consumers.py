449fee58afff6c5faf2f8271e9d2ca0344cfd117
timjonez <timjonez@protonmail.com>
2020-10-13 15:54:08 -0500

    [calculation/consumers] overwrite a few methods to get channels working correctly

--- calculator/calculations/consumers.py
+++ calculator/calculations/consumers.py
@@ -1,16 +1,41 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class CalculationConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_name = "home"
+        self.room_group_name = "index"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
         self.accept()
-    
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
     def receive(self, text_data):
-        json_calc = json.loads(text_data)
-        str_calculation = json_calc['calculation']
+        text_data_json = json.loads(text_data)
+        calc = text_data_json['calculation']
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'calculation': calc
+            }
+        )
+
+    def chat_message(self, event):
+        calc = event['calculation']
 
         self.send(text_data=json.dumps({
-            'calculation': str_calculation
+            'calculation': calc
         }))
-        
