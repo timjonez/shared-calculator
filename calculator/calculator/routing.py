62c390dee659b558bf7750ef49e45c7894af97e0
timjonez <timjonez@protonmail.com>
2020-10-13 14:21:09 -0500

    [calculator/routing] add app routing to root routing file

--- calculator/calculator/routing.py
+++ calculator/calculator/routing.py
@@ -1,3 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from calculations.routing import websocket_urlpatterns
 
-application = ProtocolTypeRouter({})
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
