d81df84ddaa923ecbe3f8ee0906d6247f55db451
timjonez <timjonez@protonmail.com>
2020-10-13 15:11:06 -0500

    [calculation/consumers] add logic to bounce back calculations

--- calculator/calculations/consumers.py
+++ calculator/calculations/consumers.py
@@ -6,7 +6,11 @@ class CalculationConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
     
-    def receive(self, calculation):
-        json_calc = json.loads(calculation)
+    def receive(self, text_data):
+        json_calc = json.loads(text_data)
         str_calculation = json_calc['calculation']
+
+        self.send(text_data=json.dumps({
+            'calculation': str_calculation
+        }))
         
