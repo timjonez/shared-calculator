1d9b940e1fc9ce2e3fa5d9a2265eb3cc7a6f15e4
timjonez <timjonez@protonmail.com>
2020-10-13 13:37:34 -0500

    [calculations/models] add model to hold calculations

--- calculator/calculations/models.py
+++ calculator/calculations/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Calculation(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=300)
